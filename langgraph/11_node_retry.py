b18882f0ef88eafc259612f6e0f164fb08b50c5b
“yan_xh” <1540719909@qq.com>
2026-06-17 11:39:33 +0800

    测试：测试分支修改文件

---
+++ langgraph/11_node_retry.py
@@ -0,0 +1,7 @@
+"""
+    测试：节点的重试
+    LangGraph不会重试的异常类型：
+    ValueError, TypeError, ArithmeticError, ImportError
+    LookupError, NameError, SyntaxError, RuntimeError
+     ReferenceError, StopIteration, StopAsyncIteration, OSError
+"""
