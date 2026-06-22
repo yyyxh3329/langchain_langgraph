9661acf1d25249653461ce83aa5b91978934c6c6
“yan_xh” <1540719909@qq.com>
2026-06-23 00:18:15 +0800

    功能：整理代码

--- app/shared/tool/download_bgem3.py
+++
@@ -1,8 +0,0 @@
-"""
-工具脚本，用于处理 download bgem3 相关的辅助任务。
-"""
-from modelscope.hub.snapshot_download import snapshot_download
-
-# 下载模型到当前目录下的 models/bge-m3 文件夹
-model_dir = snapshot_download('BAAI/bge-m3', cache_dir='D:/ai_models/modelscope_cache/models')
-print(f"模型已下载到: {model_dir}")
--- langgraph/11_node_retry.py
+++ langgraph/11_node_retry.py
@@ -5,5 +5,38 @@
     LookupError, NameError, SyntaxError, RuntimeError
      ReferenceError, StopIteration, StopAsyncIteration, OSError
 """
+from typing import TypedDict
 
-# 注释
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph
+from langgraph.types import CachePolicy, RetryPolicy
+
+
+class MyState(TypedDict):
+    result: str
+
+
+counter = 0
+
+
+def node_retry(state: MyState):
+    global counter
+    counter += 1
+    if counter <= 5:
+        print(f"当前第{counter}次重试，抛出异常")
+        raise Exception("测试节点重试")
+    else:
+        return {"result": f"当前第{counter}次重试，执行成功"}
+
+
+builder = StateGraph(MyState)
+
+builder.add_node(node_retry, retry_policy=RetryPolicy(max_attempts=3))
+
+builder.add_edge(START, "node_retry")
+builder.add_edge("node_retry", END)
+
+graph = builder.compile()
+
+result = graph.invoke({})
+print(result)
